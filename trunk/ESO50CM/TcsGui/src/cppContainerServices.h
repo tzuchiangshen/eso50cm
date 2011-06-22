56c0211c8753228d83011e392e5fc4963795027b
tzuchiangshen@gmail.com <tzuchiangshen@gmail.com@252240e4-1210-6a41-b381-b427a22c1ab4>
2011-06-22 02:57:29 +0000

    tshen 2011-06-21: module created (migrated old version to use LCUControl)
    A    TcsGui
    A    TcsGui/src
    A    TcsGui/src/cppContainerServices.h
    A    TcsGui/src/LCU.ice
    A    TcsGui/src/commandthread.cpp
    A    TcsGui/src/telescopecli.ui
    A    TcsGui/src/lcuthread.cpp
    A    TcsGui/src/commandthread.h
    A    TcsGui/src/telescopecli.cpp
    A    TcsGui/src/telescopecli.h
    A    TcsGui/src/lcuthread.h
    A    TcsGui/src/myRS232.cpp
    A    TcsGui/src/LCUClient.cpp
    A    TcsGui/src/LCU.cpp
    A    TcsGui/src/main.cpp
    A    TcsGui/src/myRS232.h
    A    TcsGui/src/LCUClient.h
    A    TcsGui/src/LCU.h
    A    TcsGui/src/cppContainerServices.cpp
    A    TcsGui/src/qtcli.pro
    A    TcsGui/src/Makefile

---
+++ trunk/ESO50CM/TcsGui/src/cppContainerServices.h
@@ -0,0 +1,52 @@
+#ifndef CS_CONTAINER_SERVICES_H
+#define CS_CONTAINER_SERVICES_H
+
+
+#include <Ice/Ice.h>
+#include "LCU.h"
+#include <QThread>
+#include <QMutex>
+#include <QDebug>
+#include <math.h>
+#include <string.h>
+
+
+using namespace std;
+using namespace OUC;
+
+class cppContainerServices : public QThread
+{
+	Q_OBJECT
+public:
+    cppContainerServices();
+    ~cppContainerServices();
+
+	void run();
+
+    int connect();
+    int disconnect();
+	LCUPrx getLCUReference();
+	void test();
+	OUC::TelescopeData getTelescopeData();
+	void getCurrentPositionRA(char *buffer, int maxlen);
+	void getCurrentPositionDec(char *buffer, int maxlen);
+	int setTargetPositionRA(const char* ra);
+	int setTargetPositionDec(const char* arguments);
+
+
+signals: 
+	void newData(const int type,  OUC::TelescopeData *data);
+private:
+    int getPosition();
+	char * strfdegs( char * string, size_t max_len, const char * format, double degs );
+	double degs2double(char *str);
+    Ice::CommunicatorPtr communicator;
+    LCUPrx lcu;
+	OUC::TelescopeData *data;
+	QMutex mutex;
+};
+#endif 
+
+
+
+
