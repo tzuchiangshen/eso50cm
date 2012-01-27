62979ff2322bbd0f3f40799bc66a4e39567fa958
tzuchiangshen@gmail.com <tzuchiangshen@gmail.com@252240e4-1210-6a41-b381-b427a22c1ab4>
2012-01-27 11:27:57 +0000

    tshen 2012-01-27: missing .h
    M    include/myTMotor.h
    M    include/myTAxis.h
    M    include/LCUImpl.h

--- branches/v244/ESO50CM/tcs/include/LCUImpl.h
+++ branches/v244/ESO50CM/tcs/include/LCUImpl.h
@@ -29,6 +29,7 @@ public:
     void setConfiguration(const string& fileName);
     virtual void setTarget(const ::OUC::TelescopePosition& targetPos, const Ice::Current&);
     virtual void setOffset(const ::OUC::TelescopePosition& offsetPos, const Ice::Current&);
+    void setOffset(const ::OUC::TelescopePosition& offsetPos);
     virtual void setTracking(const OUC::TrackingInfo& trkInfo, const Ice::Current&);
     virtual void parkTelescope(const Ice::Current&);
     virtual void stopTelescope(OUC::TelescopeDirection dir, const Ice::Current&);
