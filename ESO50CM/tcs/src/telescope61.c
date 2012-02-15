04aa4899fce59f4c7f6604abbbea0eb458143217
epiktsu <epiktsu@252240e4-1210-6a41-b381-b427a22c1ab4>
2012-02-15 18:30:53 +0000

    tshen 2012-02-15:     - added debug information for setDeviceMemory()     - re-enable stdio for telescope61

--- ESO50CM/tcs/src/telescope61.c
+++ ESO50CM/tcs/src/telescope61.c
@@ -406,17 +406,17 @@ void telescope_run( const char * device, speed_t baudrate, const char * socket_n
         /**
          * File descriptor for standard in
          */
-//        fd_stdin = fileno( stdin );
-//        if( fd_rs232 == 0 ) {
-//            init_ok_flag = 0;
-//            if( verbose )
-//                printf( "[telescope_run] fd_stdin = 0\n" );
-//        } else if( fd_stdin < 0 ) {
-//            init_ok_flag = 0;
-//            perror( "[telescope_run] fileno" );
-//            if( verbose )
-//                printf( "[telescope_run] fileno ERROR.\n" );
-//        }
+        fd_stdin = fileno( stdin );
+        if( fd_rs232 == 0 ) {
+            init_ok_flag = 0;
+            if( verbose )
+                printf( "[telescope_run] fd_stdin = 0\n" );
+        } else if( fd_stdin < 0 ) {
+            init_ok_flag = 0;
+            perror( "[telescope_run] fileno" );
+            if( verbose )
+                printf( "[telescope_run] fileno ERROR.\n" );
+        }
 
     } while( 0 );
 
@@ -708,17 +708,17 @@ void telescope_run( const char * device, speed_t baudrate, const char * socket_n
             /**
             * Get Message to TX from stdin
             */
-//            printf( "[telescope_run] checking for a stdio message...\n" );
-//            if( ! new_message_flag ) {
-//                if( ( retval = stream_status( fd_stdin ) ) > 0 ) {
-//                    memset( buffer, 0 , 128 );
-//                    retval = read( fd_stdin, buffer, 128 );
-//                    buffer[retval-1] = 0;  //enter = 0
-//                    fflush( stdin );
-//                    new_message_flag = 1;
-//                    message_length = strlen( buffer );
-//                }
-//            }//END if( ! new_message_flag )
+           // printf( "[telescope_run] checking for a stdio message...\n" );
+            if( ! new_message_flag ) {
+                if( ( retval = stream_status( fd_stdin ) ) > 0 ) {
+                    memset( buffer, 0 , 128 );
+                    retval = read( fd_stdin, buffer, 128 );
+                    buffer[retval-1] = 0;  //enter = 0
+                    fflush( stdin );
+                    new_message_flag = 1;
+                    message_length = strlen( buffer );
+                }
+            }//END if( ! new_message_flag )
 
             /**
             * Process stdin message
