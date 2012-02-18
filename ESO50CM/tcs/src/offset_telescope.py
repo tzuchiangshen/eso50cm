5f465937bae43cca64b96e61eecf3643bfdabc26
tzuchiangshen@gmail.com <tzuchiangshen@gmail.com@252240e4-1210-6a41-b381-b427a22c1ab4>
2012-02-18 14:05:45 +0000

    tshen 2012-02-18:     - added DeviceManager class for low level interface commands (set/readDeviceMemory)     - added stop_telescope.py     - adapted offset_telescope.py to use DeviceManager

--- ESO50CM/tcs/src/offset_telescope.py
+++ ESO50CM/tcs/src/offset_telescope.py
@@ -7,92 +7,15 @@ import math
 from AstroUtil import *
 import os
 import optparse
-
-#global variables
-global status 
-global ic 
-global lcuImpl 
-
-def connect():
-    global status
-    global ic
-    global lcuImpl
-
-    print "Connecting.."
-    status = 0
-    try:
-        # Reading configuration info 
-        configPath = os.environ.get("SWROOT")
-        configPath = configPath + "/config/LCU-config"
-        initData = Ice.InitializationData()
-        initData.properties = Ice.createProperties()
-        initData.properties.load(configPath)
-        ic = Ice.initialize(sys.argv, initData)
-
-        # Create proxy
-        properties = ic.getProperties();
-        proxyProperty = "LCUAdapter.Proxy"
-        proxy = properties.getProperty(proxyProperty);
-        print proxy
-        obj = ic.stringToProxy(proxy);
-        lcuImpl = OUC.LCUPrx.checkedCast(obj)
-        print "Connected to LCUControl"
-        if not lcuImpl:
-            raise RuntimeError("Invalid proxy")
-    except:
-       traceback.print_exc()
-       status = 1
-       sys.exit(status)
-
-def disconnect():
-    global status
-    print "Desconnecting.."
-    if ic:
-        try: 
-            ic.destroy()
-        except:
-            traceback.print_exc()
-            status = 1
-            sys.exit(status)
-
-def readDeviceMemory(deviceId, address, value):
-    try:
-        return lcuImpl.readDeviceMemory(deviceId, address, value)
-    except:
-        traceback.print_exc()
-        status = 1
-
-def setDeviceMemory(deviceId, address, value):
-    try:
-        return lcuImpl.setDeviceMemory(deviceId, address, value)
-    except:
-        traceback.print_exc()
-        statur = 1
-
-def offset_telescope(ra, dec):
-
-        offsetPos = OUC.TelescopePosition()
-        offsetPos.RA = sexagesimal2degs(ra, True)
-        offsetPos.Dec = sexagesimal2degs(dec, False)
-        print "setOffset ra=%f, dec=%f" % (offsetPos.RA, offsetPos.Dec)
-        try:
-            lcuImpl.setOffset(offsetPos)
-        except OUC.TargetOutOfLimitsEx():
-            print "Target Out of Limits. Try a new one\n";
-            traceback.print_exc()
-            status =1
-        except OUC.TelescopeNotConfiguredEx():
-            print "Telescope Not Configured !!!"
-            traceback.print_exc()
-            status =1
-        except ValueError:
-            print "wrong format"
+from DeviceManager import DeviceManager
  
         
 if __name__ == "__main__":
 
     parser = optparse.OptionParser(usage= "%prog rad=00:00:00 dec=00:00:00")
     (options, args) = parser.parse_args()
+
+    mgr = DeviceManager()
 
     ra = "00:00:00"
     dec = "00:00:00"
@@ -117,31 +40,31 @@ if __name__ == "__main__":
     dec = int(dec / 360.0 * 13882.0 * 6000.0)
     print "Motor Encoders alpha=%d, delta=%d"  % (ra,dec)
 
-    connect()
+    mgr.connect()
     quit = False
     quitAlpha = False
     quitDelta = False 
 
     #offset_telescope(ra, dec)
-    setDeviceMemory(0xA2, 7, ra)
-    setDeviceMemory(0xA4, 7, dec)
+    mgr.setDeviceMemory(0xA2, 7, ra)
+    mgr.setDeviceMemory(0xA4, 7, dec)
     time.sleep(1)
-    posAlpha = readDeviceMemory(0xA2, 7, 0)
-    posDelta = readDeviceMemory(0xA4, 7, 0)
+    posAlpha = mgr.readDeviceMemory(0xA2, 7, 0)
+    posDelta = mgr.readDeviceMemory(0xA4, 7, 0)
     print "alpha = %d" % posAlpha
     print "delta = %d" % posDelta
     print ""
 
     while(not quit):
         #Alpha
-        posAlpha = readDeviceMemory(0xA2, 7, 0)
+        posAlpha = mgr.readDeviceMemory(0xA2, 7, 0)
         print "alpha = %d" % posAlpha
         if (math.fabs(posAlpha) < 50.0):
             #print "listo en alpha"
             quitAlpha = True
 
         #Delta
-        posDelta = readDeviceMemory(0xA4, 7, 0)
+        posDelta = mgr.readDeviceMemory(0xA4, 7, 0)
         print "delta = %d" % posDelta
         if (math.fabs(posDelta) < 50.0): 
             #print "listo en delta"
@@ -154,5 +77,5 @@ if __name__ == "__main__":
         time.sleep(3)
         print("")
 
-    disconnect()
+    mgr.disconnect()
     
