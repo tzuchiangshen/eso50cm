f8f39e44299ee4942c7a40f847bedace0cb2dd4c
tzuchiangshen@gmail.com <tzuchiangshen@gmail.com@252240e4-1210-6a41-b381-b427a22c1ab4>
2012-02-19 19:57:57 +0000

    tshen 2012-02-18: added set_tracking.py and calculate_tracking_rate.py cmd line tools

---
+++ branches/eso50cm-stable-logging/ESO50CM/tcs/src/set_tracking.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python 
+import sys, traceback, Ice 
+import OUC
+from datetime import datetime
+import time
+import math
+from AstroUtil import *
+import os
+import optparse
+from DeviceManager import DeviceManager
+
+if __name__ == "__main__":
+
+    parser = optparse.OptionParser(usage= "%prog")
+    (options, args) = parser.parse_args()
+    print args
+
+    mgr = DeviceManager()
+
+    mgr.connect()
+
+    if(len(args) == 0):
+        print "usage: set_tracking [on/off]"
+        sys.exit()
+
+    val = args[0]
+    val = val.upper()
+
+    if(val == 'ON'):
+        print "start tracking"
+        mgr.setDeviceMemory(0xA2, 3, 1)
+    elif(val == 'OFF'):
+        print "stop tracking"
+        mgr.setDeviceMemory(0xA2, 3, 0)
+    else: 
+        print "usage: set_tracking [on/off]"
+
+    mgr.disconnect()
+    
