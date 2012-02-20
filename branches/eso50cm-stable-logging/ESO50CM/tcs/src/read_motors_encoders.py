a726f0e8973c10020acaab43fda9bb7f98cf1817
tzuchiangshen@gmail.com <tzuchiangshen@gmail.com@252240e4-1210-6a41-b381-b427a22c1ab4>
2012-02-20 12:31:21 +0000

    tshen 2012-02-20: added troubleshooting tool read_motor_encoders.py whichs return the remaining encoders to move v/s current motor encoder positions

---
+++ branches/eso50cm-stable-logging/ESO50CM/tcs/src/read_motors_encoders.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python 
+import sys, traceback, Ice 
+import OUC
+from datetime import datetime
+import time
+import math
+from AstroUtil import *
+import os
+import optparse
+from DeviceManager import DeviceManager
+
+if __name__ == "__main__":
+
+    parser = optparse.OptionParser(usage= "%prog")
+    (options, args) = parser.parse_args()
+
+    mgr = DeviceManager()
+
+    mgr.connect()
+
+    AlphaMotorRemaining = mgr.readDeviceMemory(0xA2,7,0)
+    AlphaMotorCurrent = mgr.readDeviceMemory(0xA2,2,0)
+    DeltaMotorRemaining = mgr.readDeviceMemory(0xA4,7,0) 
+    DeltaMotorCurrent  = mgr.readDeviceMemory(0xA4,2,0)
+
+    print chr(27)+"[0;33m"+"                   Remaining     Current " + chr(27) + "[0m"
+    print                  "Alpha MotorE  = [%+8.0lf     %+8.0lf ]" % (AlphaMotorRemaining, AlphaMotorCurrent)
+    print                  "Delta MotorE  = [%+8.0lf     %+8.0lf ]" % (DeltaMotorRemaining, DeltaMotorCurrent)
+    print chr(27)+"[0m" 
+
+    mgr.disconnect()
+    
