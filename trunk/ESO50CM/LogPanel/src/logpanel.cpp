d59ba68ee888f816b11b7eb242d2c2bd6927eae7
epiktsu <epiktsu@252240e4-1210-6a41-b381-b427a22c1ab4>
2012-04-20 18:07:54 +0000

    added new LogPanel

---
+++ trunk/ESO50CM/LogPanel/src/logpanel.cpp
@@ -0,0 +1,59 @@
+#include "logpanel.h"
+#include "ui_logpanel.h"
+#include <string>
+#include <vector>
+
+
+
+
+LogPanel::LogPanel(QWidget *parent)
+    : QWidget(parent), ui(new Ui::LogPanel)
+{
+    ui->setupUi(this);
+    //verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+    //ui->sourceWidgetLayou->addItem(verticalSpacer);
+    list=new QList<LogMessageQT>;
+    model=new MessageTableModel(this);
+    model->setList(list,100000);  //   <--- MAX NUMBER OF LOGS!!
+    proxyModel = new QSortFilterProxyModel(this);
+    proxyModel->setSourceModel(model);
+    proxyModel->setFilterKeyColumn(1);
+    setProxyFilter(2); // default value for this is info
+    connect(ui->cbFilter,SIGNAL(currentIndexChanged(int)),this,SLOT(setProxyFilter(int)));
+    ui->tableView->setModel(proxyModel);
+    proxyModel->sort(0, Qt::DescendingOrder);
+}
+void LogPanel::setProxyFilter( int filter)
+{
+    switch(filter) {
+      case 0:
+            proxyModel->setFilterRegExp("(^Severe$)"); return;
+      case 1:
+            proxyModel->setFilterRegExp("(^Severe$|^Warning$)"); return;
+      case 2:
+            proxyModel->setFilterRegExp("(^Severe$|^Warning$|^Info$)"); return;
+      case 3:
+            proxyModel->setFilterRegExp("(^Severe$|^Warning$|^Info$|^Config$)"); return;
+      case 4:
+            proxyModel->setFilterRegExp("(^Severe$|^Warning$|^Info$|^Config$|^Fine$)"); return;
+      case 5:
+            proxyModel->setFilterRegExp("(^Severe$|^Warning$|^Info$|^Config$|^Fine$|^Finer$)"); return;
+      case 6:
+            proxyModel->setFilterRegExp("(^Severe$|^Warning$|^Info$|^Config$|^Fine$|^Finer$|^Finest$)"); return;
+    }
+}
+LogPanel::~LogPanel()
+{
+    delete ui;
+    delete model;
+    delete list;
+}
+
+
+void LogCapturerI::logEvent(const LogMessageData &message, const Ice::Current& c) {
+    if (gui) {
+        gui->model->addMessage(message);
+   }
+}
+
+
